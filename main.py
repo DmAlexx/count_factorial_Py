fcce01be226655d8937534158c645b22cd20407b
Dmitry <xedos0901@gmail.com>
2023-08-26 08:43:31 +0300

    second version

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-number = int(input("Enter any number to count factorial N: "))
-factorial = 1
+while True:
+    number = int(input("Enter any number above zero to count factorial N: or 0 to exit "))
+    factorial = 1
 
-for i in range(1, number + 1):
-    factorial *= i
-
-print(f"Factorial of {number} = {factorial}")
+    if number >= 1:
+        for i in range(1, number + 1):
+            factorial *= i
+        print(f"Factorial of {number} = {factorial}")
+    elif number < 0:
+        print("Wrong number!")
+    elif number == 0:
+        break
